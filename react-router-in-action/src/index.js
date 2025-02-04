28d11149519f5a9038f72166ad39fec5ff39dfa3
zss0527 <zss0527@outlook.com>
2025-02-04 13:47:30 +0800

    别名路径配置

--- react-router-in-action/src/index.js
+++ react-router-in-action/src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import sum from "@/test";
+
+console.log(sum(1, 3));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
