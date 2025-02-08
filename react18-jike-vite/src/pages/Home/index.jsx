e3640d48b370a88ed9a420a6b52d2eb7c881b501
zss0527 <zss0527@outlook.com>
2025-02-08 20:45:24 +0800

    echarts初始化

--- react18-jike-vite/src/pages/Home/index.jsx
+++ react18-jike-vite/src/pages/Home/index.jsx
@@ -1,5 +1,32 @@
+import * as echarts from 'echarts';
+import { useEffect, useRef } from 'react';
+
 const Home = () => {
-  return <div> home page works!</div>
+  const chartRef = useRef(null)
+  useEffect(() => {
+    // const chartDom = document.getElementById('echarts');
+    const myChart = echarts.init(chartRef.current);
+    const option = {
+      xAxis: {
+        type: 'category',
+        data: ['Vue', 'Angular', 'React']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [120, 200, 150],
+          type: 'bar'
+        }
+      ]
+    };
+    option && myChart.setOption(option);
+  }, [])
+
+  return <>
+    <div ref={chartRef} style={{ width: '500px', height: '400px' }}></div>
+  </>
 }
 
 export default Home
