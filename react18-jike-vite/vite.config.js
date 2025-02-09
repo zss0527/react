7cfc5e179b33c04f5c03edc43d5dce263b29381b
zss0527 <zss0527@outlook.com>
2025-02-09 19:26:10 +0800

    vite打包可视化工具

--- react18-jike-vite/vite.config.js
+++ react18-jike-vite/vite.config.js
@@ -2,12 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { fileURLToPath } from 'url'
+// import visualizer from 'rollup-plugin-visualizer';
+// import { bundleVisualizer } from 'vite-bundle-visualizer'
 
 // https://vite.dev/config/
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // visualizer({
+    //   open: true,//打包结束后自动打开报告
+    //   filename: 'bundle-analysis.html',//报告文件名
+    // }),
+    // bundleVisualizer({
+    //   open: false,
+    //   filename: 'bundle-analysis.html',
+    // })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
